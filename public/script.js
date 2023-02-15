4f502c2a4b7320e1809e1357c4bb9eab6d30bdfc
Glitch (glitch-hello-website) <none>
2023-02-15 20:11:23 +0000

    🏩🌘 Checkpoint ./public/script.js:53427628/2181 ./views/start.hbs:53427628/1090 ./public/style.css:53427628/489 ./views/partials/menu.hbs:53427628/768 ./views/layouts/main.hbs:53427628/159

---
+++ public/script.js
@@ -0,0 +1,31 @@
+const greenbtn = document.querySelector(".green");
+greenbtn &&
+  greenbtn.addEventListener("click", () => alert("Thanks! You're okay too"));
+
+const bluebtn = document.querySelector(".blue");
+bluebtn &&
+  bluebtn.addEventListener("click", () => {
+      let readMoreDiv = document.querySelector("#readmore");
+      if (readMoreDiv.style.display === "block") {
+        readMoreDiv.style.display = "none";
+      } else {
+        readMoreDiv.style.display = "block";
+      }
+  });
+
+const redbtn = document.querySelector(".red");
+redbtn &&
+  redbtn.addEventListener("click", () => {
+    let username = prompt("What's your name?");
+    let welcomeUserDiv = document.querySelector("#welcomeuser");
+    welcomeUserDiv.style.display = "block";
+    document.querySelector("#welcomeuser").innerHTML 
+      = `<p> Hello, ${username}, looking forward to hearing your playlists!. Click this message to close it.</p>`;
+    welcomeUserDiv.style.cursor = "pointer";
+  });
+
+const hideDiv = document.querySelector("#welcomeuser");
+hideDiv &&
+  hideDiv.addEventListener("click", () => {
+    hideDiv.style.display = "none"
+  })
